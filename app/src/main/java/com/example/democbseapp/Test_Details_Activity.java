cd8f552737a6c49c6ca6b0343a95ccb5ad0b1819
VaishnaviKairamkonda <vaishnavikairamkonda@gmail.com>
2021-10-05 19:21:41 +0530

    Intial Commit

---
+++ app/src/main/java/com/example/democbseapp/Test_Details_Activity.java
@@ -0,0 +1,14 @@
+package com.example.democbseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test_Details_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_details);
+    }
+}
---
+++ app/src/main/java/com/example/democbseapp/VisitClass_Report_Activity.java
@@ -0,0 +1,14 @@
+package com.example.democbseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitClass_Report_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visit_class_report);
+    }
+}
